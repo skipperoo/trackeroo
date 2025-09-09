ac80b0232033699a2e795e627b2f7a4fc00a27e4
Simone <simostanga@libero.it>
2025-09-09 12:20:03 +0200

    kube init

---
+++ tsdb/docker-entrypoint-initdb.d/02-schema.sql
@@ -0,0 +1,34 @@
+CREATE SCHEMA IF NOT EXISTS trackeroo;
+
+CREATE TABLE IF NOT EXISTS trackeroo.data (
+    ts_unix BIGINT NOT NULL,
+    ts TIMESTAMPTZ NOT NULL,
+    dev_id TEXT NOT NULL,
+    insertion_time TIMESTAMPTZ DEFAULT NOW(),
+    tag TEXT NOT NULL,
+    payload JSONB NOT NULL,
+    PRIMARY KEY (ts, dev_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS trackeroo.aggregated (
+    ts_unix BIGINT NOT NULL,
+    ts TIMESTAMPTZ NOT NULL,
+    dev_id TEXT NOT NULL,
+    insertion_time TIMESTAMPTZ DEFAULT NOW(),
+    tag TEXT NOT NULL,
+    payload JSONB NOT NULL,
+
+    PRIMARY KEY (ts, dev_id)
+);
+
+SELECT create_hypertable('trackeroo.data', 'ts');
+SELECT create_hypertable('trackeroo.aggregated', 'ts');
+
+CREATE INDEX IF NOT EXISTS idx_trackeroo_data_dev_id ON trackeroo.data(dev_id);
+CREATE INDEX IF NOT EXISTS idx_trackeroo_data_tag ON trackeroo.data(tag);
+CREATE INDEX IF NOT EXISTS idx_trackeroo_data_ts ON trackeroo.data(ts);
+
+CREATE INDEX IF NOT EXISTS idx_trackeroo_aggregated_dev_id ON trackeroo.aggregated(dev_id);
+CREATE INDEX IF NOT EXISTS idx_trackeroo_aggregated_tag ON trackeroo.aggregated(tag);
+CREATE INDEX IF NOT EXISTS idx_trackeroo_aggregated_ts ON trackeroo.aggregated(ts);
