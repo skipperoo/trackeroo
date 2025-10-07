18b0300d2aa2f53537384efeecde7e0dbf18ad33
Skiby7 <l.scoppitto@studenti.unipi.it>
2025-10-07 15:10:56 +0200

    fixing tsdb

---
+++ services/playbooks/roles/tsdb/files/01-init.sql
@@ -0,0 +1,32 @@
+CREATE EXTENSION IF NOT EXISTS timescaledb;
+CREATE EXTENSION IF NOT EXISTS postgis;
+CREATE EXTENSION IF NOT EXISTS postgis_topology;
+DO
+$$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'admin') THEN
+      CREATE ROLE admin
+        WITH LOGIN SUPERUSER CREATEDB CREATEROLE REPLICATION BYPASSRLS
+        PASSWORD 'administrator';
+   END IF;
+END
+$$;
+
+-- Application role
+DO
+$$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'apps') THEN
+      CREATE ROLE apps
+        WITH LOGIN PASSWORD 'apps';
+   END IF;
+END
+$$;
+GRANT ALL PRIVILEGES ON DATABASE tracker_db TO admin;
+
+-- TimescaleDB specific permissions
+GRANT USAGE ON SCHEMA _timescaledb_catalog TO apps;
+GRANT USAGE ON SCHEMA _timescaledb_config TO apps;
+GRANT USAGE ON SCHEMA _timescaledb_internal TO apps;
+GRANT SELECT ON ALL TABLES IN SCHEMA _timescaledb_catalog TO apps;
+GRANT SELECT ON ALL TABLES IN SCHEMA _timescaledb_config TO apps;
---
+++ services/playbooks/roles/tsdb/files/02-schema.sql
@@ -0,0 +1,53 @@
+CREATE SCHEMA IF NOT EXISTS trackeroo;
+
+CREATE TABLE IF NOT EXISTS trackeroo.data (
+    ts_unix BIGINT NOT NULL,
+    ts TIMESTAMPTZ NOT NULL,
+    dev_id TEXT NOT NULL,
+    insertion_time TIMESTAMPTZ DEFAULT NOW(),
+    tag TEXT NOT NULL,
+    payload JSONB NOT NULL,
+    PRIMARY KEY (ts, dev_id)
+);
+
+
+CREATE TABLE IF NOT EXISTS trackeroo.aggregated (
+    ts_unix BIGINT NOT NULL,
+    ts TIMESTAMPTZ NOT NULL,
+    dev_id TEXT NOT NULL,
+    route_hash TEXT NOT NULL,
+    insertion_time TIMESTAMPTZ DEFAULT NOW(),
+    tag TEXT NOT NULL,
+    payload JSONB NOT NULL,
+
+    PRIMARY KEY (ts, route_hash, dev_id)
+);
+
+SELECT create_hypertable('trackeroo.data', 'ts', 'dev_id', 16);
+SELECT create_hypertable('trackeroo.aggregated', 'ts', 'dev_id', 16);
+
+CREATE INDEX IF NOT EXISTS idx_trackeroo_data_dev_id ON trackeroo.data(dev_id);
+CREATE INDEX IF NOT EXISTS idx_trackeroo_data_tag ON trackeroo.data(tag);
+CREATE INDEX IF NOT EXISTS idx_trackeroo_data_ts ON trackeroo.data(ts);
+
+CREATE INDEX IF NOT EXISTS idx_trackeroo_aggregated_dev_id ON trackeroo.aggregated(dev_id);
+CREATE INDEX IF NOT EXISTS idx_trackeroo_aggregated_route_hash ON trackeroo.aggregated(route_hash);
+CREATE INDEX IF NOT EXISTS idx_trackeroo_aggregated_tag ON trackeroo.aggregated(tag);
+CREATE INDEX IF NOT EXISTS idx_trackeroo_aggregated_ts ON trackeroo.aggregated(ts);
+
+
+-- Now I have to allow apps to read/write on all current tables
+GRANT USAGE ON SCHEMA trackeroo TO apps;
+--
+-- Grant read/write on all current tables
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA trackeroo TO apps;
+
+-- Grant read/write on all sequences (needed if you ever add SERIAL/IDENTITY columns)
+GRANT USAGE, SELECT, UPDATE ON ALL SEQUENCES IN SCHEMA trackeroo TO apps;
+
+-- Ensure future tables/sequences also inherit these permissions
+ALTER DEFAULT PRIVILEGES IN SCHEMA trackeroo
+   GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO apps;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA trackeroo
+   GRANT USAGE, SELECT, UPDATE ON SEQUENCES TO apps;
