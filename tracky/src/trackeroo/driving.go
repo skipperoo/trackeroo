fae3ff25032d3c64750bfa1b04481c5fd284f092
Skiby7 <l.scoppitto@studenti.unipi.it>
2025-10-16 16:05:24 +0200

    checkpointing

--- tracky/src/trackeroo/driving.go
+++ tracky/src/trackeroo/driving.go
@@ -186,7 +186,7 @@ func NewDrivingSimulator(routingService *RoutingService, checkpoint *Checkpoint,
 			if checkpoint == nil {
 				break
 			}
-			if checkpoint.LastPosition != pos.Coordinates[0] {
+			if checkpoint.LastPosition == pos.Coordinates[0] {
 				checkpointIndex = i
 				Info("Found checkpoint %+v at index %d", checkpoint, checkpointIndex)
 				break
