034b5e93e0dba6588f5dc4e9c639e7f815ae0fbd
Skiby7 <l.scoppitto@studenti.unipi.it>
2025-10-17 15:15:27 +0200

    redis checkpoint

--- tracky/make_compose.py
+++ tracky/make_compose.py
@@ -44,7 +44,7 @@ def create_compose(credentials: List[Dict]):
         services={},
         secrets={},
         # networks={"trackynet": NetworkConfig(driver="overlay", attachable=True)},
-        networks={"trackynet": NetworkConfig(external=True, name="tracky")},
+        networks={"tracky": NetworkConfig(external=True)},
     )
     print("SELECT * FROM ( VALUES ")
     for i, cred in enumerate(credentials):
